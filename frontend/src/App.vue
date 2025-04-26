018e8014011b85ba36d4536e29cf83d2b1068549
goodday <whtjddbs0112@naver.com>
2025-04-26 21:30:18 +0900

    vue router 설정 완료

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
-<script setup></script>
-
-<template></template>
-
-<style scoped></style>
+<template>
+  <h1>Hello App!</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/Profile">Go to Profile</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Home Page 입니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
