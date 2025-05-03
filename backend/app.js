634cdcd2bd02cc1beae364a49fa3ffd6c7287c8d
goodday <whtjddbs0112@naver.com>
2025-05-03 22:29:56 +0900

    16강 - 백엔드 프로젝트 초기화 및 필수 의존성 설치

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 미들웨어 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+app.get("/", (req, res) => {
+    res.send(" Express API 서버가 실행 중입니다.");
+});
+
+app.listen(PORT, () => {
+    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+});
